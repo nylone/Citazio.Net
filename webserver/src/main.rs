8218bbe8fd729e6b54f2ea12a8f19d5e4b6c17c3
Matteo Rampone <dev@rampeo.com>
2022-10-04 19:14:38 +0000

    finalized cli args parsing with clap (#4)

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,36 +1,41 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr};
 
 use anyhow::*;
-use axum::{
-    Router, 
-    routing::{get, post},
-    Extension};
+use axum::{routing::post, Extension, Router};
+use clap::Parser;
 
+mod cli;
 mod database;
 mod handlers;
 mod parser;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // DB url needs to be extracted from a config file, for now this is the spec
-    parser::parse_config("/home/tommaso/Documents/Hacktoberfest/theysa.id/webserver/src/test.toml")?;
-    /* let db_url = "mysql://theysa_user:theysa_pass@localhost:3306/theysa_db";
-    let db = database::DbWrapper::new(db_url).await?;
+    // parse the command line arguments
+    let args = cli::Args::parse();
+    match args.mode {
+        cli::OperationMode::StartServer => {
+            todo!("starts the server");
+            // DB url needs to be extracted from a config file, for now this is the spec
+            let db_url = "mysql://theysa_user:theysa_pass@localhost:3306/theysa_db";
+            let db = database::DbWrapper::new(db_url).await?;
 
-    // build our application with some routes
-    let app = Router::new()
-        .route(
-            "/",
-            post(handlers::signup))
-        .layer(Extension(db));
+            // build our application with some routes
+            let app = Router::new()
+                .route("/auth/register", post(handlers::signup))
+                .layer(Extension(db));
+            // run it with hyper
+            let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+            axum::Server::bind(&addr)
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        }
+        cli::OperationMode::AddToken => {
+            todo!("adds a token");
+        }
+    }
 
-    // run it with hyper
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-    */
 
     Ok(())
 }
