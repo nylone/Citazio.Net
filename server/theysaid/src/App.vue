996df08df465c4960d549b37d1c75f4b62f1742f
slashtube <tommy.ferpet6@gmail.com>
2023-01-29 21:56:36 +0100

    Started Vue js implementation

---
+++ server/theysaid/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <navbar/>
+  
+  <auth />
+</template>
+
+<script>
+import auth from './components/auth.vue'
+import navbar from './components/navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navbar,
+    auth,
+    
+  }
+}
+</script>
---
+++ server/theysaid/src/components/navbar.vue
@@ -0,0 +1,14 @@
+<script>
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<template>
+    <h1 class="theysa-shadow">theysa.id</h1>
+    <nav class="theysa-box theysa-flex-row theysa-navbar theysa-shadow grows">
+        <a href="#" class="">Home</a>
+        <a href="#" class="">Authenticate</a>
+        <a href="#" class="">About</a>
+    </nav>
+</template>
