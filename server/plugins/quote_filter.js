76eae6c2672412f4450674bcece055ac8bf385e5
rampeo <rampeo.mattone@gmail.com>
2023-01-30 23:57:56 +0100

    fixed filter

--- server/plugins/quote_filter.js
+++ server/plugins/quote_filter.js
@@ -3,8 +3,16 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify, opts, done) {
+
+  const uname_len = 32;
+  const text_len = 4096;
+
   function string_constraint(s, len) {
     return typeof (s) === "string" && String.length(s) <= len ? s : undefined;
+  }
+
+  function date_constraint(d) {
+    return d === parseInt(data, 10) ? d : undefined;
   }
 
   function filter(input) {
@@ -15,13 +23,13 @@ module.exports = fp(async function (fastify, opts, done) {
     return {
       array: input.array.map(x => {
         return {
-          msg: string_constraint(x.msg, 256),
-          by: string_constraint(x.by, 32),
-          ctx: string_constraint(x.ctx, 256),
+          msg: string_constraint(x.msg, text_len),
+          by: string_constraint(x.by, uname_len),
+          ctx: string_constraint(x.ctx, text_len),
         };
       }).filter(x => "msg" in x),
-      ctx: string_constraint(input.ctx, 256),
-      date: string_constraint(input.date, 32),
+      ctx: string_constraint(input.ctx, text_len),
+      date: date_constraint(input.date),
     }
   }
 
