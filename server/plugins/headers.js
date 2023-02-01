d03f053031355ea673a314a350a6daef8578d974
rampeo <rampeo.mattone@gmail.com>
2023-02-01 21:45:20 +0100

    added more cors headers

--- server/plugins/headers.js
+++ server/plugins/headers.js
@@ -6,5 +6,15 @@ const cors = require("@fastify/cors");
 
 module.exports = fp(async function (fastify, opts) {
   await fastify.register(helmet, { contentSecurityPolicy: false });
-  await fastify.register(cors, { origin: "*" });
+  await fastify.register(cors, {
+    origin: "*",
+    allowedHeaders: [
+      "Content-Type",
+      "Content-Length",
+      "Authorization",
+      "Accept",
+      "X-Requested-With",
+    ],
+    methods: ["GET", "POST"],
+  });
 });
