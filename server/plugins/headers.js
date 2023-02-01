4a8648202bc6741fd1488bd3cbd5a2368e372e6c
rampeo <rampeo.mattone@gmail.com>
2023-02-01 17:46:28 +0100

    added cors headers

---
+++ server/plugins/headers.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const fp = require('fastify-plugin');
+const helmet = require('@fastify/helmet');
+const cors = require('@fastify/cors');
+
+module.exports = fp(async function (fastify, opts) {
+  await fastify.register(
+    helmet, { contentSecurityPolicy: false }
+  );
+  await fastify.register(
+    cors, { origin: "*" }
+  )
+})
