3ebd232ee78f14a0958d778a8c47cab5f1a06634
rampeo <rampeo.mattone@gmail.com>
2023-02-02 21:42:10 +0100

    removed unnecessary library + added config file options for rate limiting

--- server/plugins/headers.js
+++ server/plugins/headers.js
@@ -2,9 +2,7 @@
 
 const fp = require("fastify-plugin");
 const helmet = require("@fastify/helmet");
-const cors = require("@fastify/cors");
 
 module.exports = fp(async function (fastify, opts) {
   await fastify.register(helmet, {});
-  await fastify.register(cors, {});
 });
--- server/plugins/rate_limit.js
+++ server/plugins/rate_limit.js
@@ -3,17 +3,21 @@
 const fp = require("fastify-plugin");
 
 module.exports = fp(async function (fastify, opts) {
-  await fastify.register(import("@fastify/rate-limit"), {
-    global: true,
-    max: 5,
-    timeWindow: 1000,
-  });
-  fastify.setNotFoundHandler(
-    {
-      preHandler: fastify.rateLimit(),
-    },
-    function (request, reply) {
-      reply.notFound();
-    }
-  );
+  const config = fastify.config.server.rateLimit;
+
+  if (config.enabled) {
+    await fastify.register(import("@fastify/rate-limit"), {
+      global: true,
+      max: config.maxRequests,
+      timeWindow: config.timeWindow,
+    });
+    fastify.setNotFoundHandler(
+      {
+        preHandler: fastify.rateLimit(),
+      },
+      function (request, reply) {
+        reply.notFound();
+      }
+    );
+  }
 });
