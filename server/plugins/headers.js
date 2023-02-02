51095d1fae5468bd7875c30d33205bb311466737
rampeo <rampeo.mattone@gmail.com>
2023-02-02 14:54:06 +0100

    slimmed down header modifications

--- server/plugins/headers.js
+++ server/plugins/headers.js
@@ -5,17 +5,9 @@ const helmet = require("@fastify/helmet");
 const cors = require("@fastify/cors");
 
 module.exports = fp(async function (fastify, opts) {
-  await fastify.register(helmet, { contentSecurityPolicy: false });
+  await fastify.register(helmet, {});
   await fastify.register(cors, {
     origin: true,
-    allowedHeaders: [
-      "Content-Type",
-      "Content-Length",
-      "Authorization",
-      "Accept",
-      "X-Requested-With",
-    ],
     credentials: true,
-    methods: ["GET", "POST", "OPTIONS"],
   });
 });
