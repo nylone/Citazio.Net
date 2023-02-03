18471a1e457316127e4ccfbe5329b2ec52f54c69
rampeo <rampeo.mattone@gmail.com>
2023-02-03 12:37:32 +0100

    feat(config + api)!: added cors setting

--- server/plugins/headers.js
+++ server/plugins/headers.js
@@ -2,7 +2,14 @@
 
 const fp = require("fastify-plugin");
 const helmet = require("@fastify/helmet");
+const cors = require("@fastify/cors");
 
 module.exports = fp(async function (fastify, opts) {
   await fastify.register(helmet, {});
+  if (fastify.config.server.crossOrigin) {
+    await fastify.register(cors, {
+      origin: true,
+      credentials: true,
+    });
+  }
 });
