717d6666c8181056651b4f263c406415bc37f6b6
= <=>
2023-02-10 17:06:37 +0100

    feat(backend): csp can now be toggled

--- server/plugins/headers.js
+++ server/plugins/headers.js
@@ -5,7 +5,7 @@ const helmet = require("@fastify/helmet");
 const cors = require("@fastify/cors");
 
 module.exports = fp(async function (fastify, opts) {
-  await fastify.register(helmet, {});
+  await fastify.register(helmet, { contentSecurityPolicy: fastify.config.server.csp });
   if (fastify.config.server.crossOrigin) {
     await fastify.register(cors, {
       origin: true,
