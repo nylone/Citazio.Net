5d9a2fcf55fe0dc2eff3341604d0039897177a66
slashtube <tommy.ferpet6@gmail.com>
2023-06-27 20:28:01 +0200

    fix(frontend): Fixed dismiss button not showing properly

--- frontend/src/components/User/cookiebanner.vue
+++ frontend/src/components/User/cookiebanner.vue
@@ -3,9 +3,10 @@
         <b-alert 
             variant="warning" 
             show 
-            dismissible 
+            dismissible
             class="position-fixed fixed-bottom m-0 rounded-0" 
-            style="z-index: 2000;"> 
+            style="z-index: 2000;"
+        > 
             This site uses cookies to function. <br />
             All cookies used are for technical purposes and will not be used for purposes outside the fruition of this service.
         </b-alert>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue, IconsPlugin, ModalPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import { ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/style.scss'
 
 
-
-
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-Vue.use(ModalPlugin)
 Vue.prototype.$user = ref("")
 Vue.prototype.$path = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port
 Vue.config.productionTip = false
